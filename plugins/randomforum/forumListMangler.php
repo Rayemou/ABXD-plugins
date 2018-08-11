e765c6f4ecb7f12eeded1fa7be2f1ce0c7cb5aff
Rayemou <42294217+Rayemou@users.noreply.github.com>
2018-08-11 11:33:07 +0100

    Provisional fix for randomforum plugin
    I have no idea if this will or won't work on forums in subdirectories, but fixed 2 typing errors and used absolute paths instead of relying on a nonexistent plugin setting.

--- plugins/randomforum/forumListMangler.php
+++ plugins/randomforum/forumListMangler.php
@@ -1,11 +1,11 @@
 <?php
 
-if (!Settings::pluginGet('forumid')) continue;
+//if (!Settings::pluginGet('forumid')) continue;
 if($forum['id'] == Settings::pluginGet('forumid'))
 {
-	$rndTitles = file_get_contents("./plugins/".Settings::pluginGet('dir']."/titles.txt");
+	$rndTitles = file_get_contents("./plugins/randomforum/titles.txt");
 	$rndTitles = explode("\n", $rndTitles);
-	$rndDescs = file_get_contents("./plugins/".Settings::pluginGet('dir']."/descs.txt");
+	$rndDescs = file_get_contents("./plugins/randomforum/descs.txt");
 	$rndDescs = explode("\n", $rndDescs);
 
 	$forum['title'] = $rndTitles[array_rand($rndTitles)];
